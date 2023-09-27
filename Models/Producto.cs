277acab4f3621a65ced3e2a339a8ff5259dfa765
jair.rueda <laboratorio@UPOAULA10514>
2023-09-27 10:02:15 -0500

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿namespace app1Clases.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set;}
+        public string Descipcion { get; set; }
+        public int Cantidad { get; set; }
+
+        public Producto(int idProducto, string nombre, string descipcion, int cantidad)
+        {
+            IdProducto = idProducto;
+            Nombre = nombre;
+            Descipcion = descipcion;
+            Cantidad = cantidad;
+        }
+       
+    }
+}
---
+++ Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using app1Clases.Models;
+
+namespace app1Clases.Utils
+{
+    public class Utils
+    {
+        static public List<Producto> ListaProductos = new List<Producto>()
+        {
+            new Producto(1,"Leche", "Marca Andina", 1),
+            new Producto(2,"Arroz", "Descripcion 2", 2),
+            new Producto(3,"Huevos", "Descripcion 3", 3),
+            new Producto(4,"Pan", "Descripcion 4", 4),
+
+        };
+    }
+    
+}
